0bac235fd7cbff4fe5be77c152088196e16de601
dsamsonov <dsamsonov@ozon.ru>
2021-07-11 23:24:45 +0300

    [new_edges_to_old_nodes] new test area

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 const (
-	cellSize = 31
+	cellSize = 41
 	xMult    = 2
 )
 
@@ -91,30 +91,40 @@ func Random(count int) func(g *cell.Graph, m *cell.Map) {
 	}
 }
 
-var features = []*cell.Display{
-	{
-		Row:  15,
-		Col:  15,
-		Sign: cell.SignObstacle,
-	},
-	{
-		Row:  15,
-		Col:  14,
-		Sign: cell.SignObstacle,
-	},
-	{
-		Row:  16,
-		Col:  14,
-		Sign: cell.SignObstacle,
-	},
-	{
-		Row:  16,
-		Col:  15,
-		Sign: cell.SignObstacle,
-	},
+func drawDown(sign cell.SignType, startRow, col, distance int) []*cell.Display {
+	res := make([]*cell.Display, 0, distance)
+	for i := 1; i <= distance; i++ {
+		res = append(res, &cell.Display{
+			Row:  startRow + i,
+			Col:  col,
+			Sign: sign,
+		})
+	}
+
+	return res
+}
+
+func drawRight(sign cell.SignType, row, startCol, distance int) []*cell.Display {
+	res := make([]*cell.Display, 0, distance)
+	for i := 1; i <= distance; i++ {
+		res = append(res, &cell.Display{
+			Row:  row,
+			Col:  startCol + i,
+			Sign: sign,
+		})
+	}
+
+	return res
 }
 
 func main() {
+	features := []*cell.Display{}
+	features = append(features, drawDown(cell.SignObstacle, 5, 13, 21)...)
+	features = append(features, drawDown(cell.SignObstacle, 5, 29, 21)...)
+	features = append(features, drawRight(cell.SignObstacle, 5, 12, 17)...)
+	features = append(features, drawRight(cell.SignObstacle, 27, 12, 6)...)
+	features = append(features, drawRight(cell.SignObstacle, 27, 23, 6)...)
+
 	m := machine.NewMachine(cellSize, xMult, Alive(), features)
 
 	m.Run()
--- machine/machine.go
+++ machine/machine.go
@@ -24,7 +24,7 @@ func NewMachine(cellSize, xMult int, seed func(*cell.Graph, *cell.Map), features
 func (m *Machine) Run() {
 	m.drawer.Draw(m.Cell.State.Map)
 
-	t := time.NewTicker(time.Millisecond * 100)
+	t := time.NewTicker(time.Millisecond * 500)
 	defer t.Stop()
 
 	for {
